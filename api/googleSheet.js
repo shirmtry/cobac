0896883c01ffed88bf6920db76b69eff724cd28d
yuhnat77 <dinhtanhuy547@gmail.com>
2025-06-01 14:41:05 +0700

    Update googleSheet.js

--- api/googleSheet.js
+++ api/googleSheet.js
@@ -1,15 +1,37 @@
-import { google } from 'googleapis';
+const { google } = require('googleapis');
+const sheets = google.sheets('v4');
+const key = require('../credentials.json'); // Đường dẫn file service account
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const SHEET_ID = '1_fvM8R8nmyY0WeYdJwBveYRxoFp3P6nIsa862X5GlCQ'; // Google Sheet ID
 
-export async function getSheetsClient() {
-  const scopes = ['https://www.googleapis.com/auth/spreadsheets'];
-  const email = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
-  const key = (process.env.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, '\n');
-
-  if (!email || !key) {
-    throw new Error('Missing GOOGLE_SERVICE_ACCOUNT_EMAIL or GOOGLE_PRIVATE_KEY in environment variables.');
-  }
-
-  const jwt = new google.auth.JWT(email, null, key, scopes);
-  await jwt.authorize();
-  return google.sheets({ version: 'v4', auth: jwt });
+function getAuth() {
+  return new google.auth.JWT(
+    key.client_email,
+    null,
+    key.private_key,
+    SCOPES
+  );
 }
+
+async function getSheet(sheetName, range) {
+  const auth = getAuth();
+  const res = await sheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId: SHEET_ID,
+    range: `${sheetName}!${range}`
+  });
+  return res.data.values;
+}
+
+async function appendSheet(sheetName, values) {
+  const auth = getAuth();
+  await sheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId: SHEET_ID,
+    range: sheetName,
+    valueInputOption: 'USER_ENTERED',
+    resource: { values: [values] }
+  });
+}
+
+module.exports = { getSheet, appendSheet };
